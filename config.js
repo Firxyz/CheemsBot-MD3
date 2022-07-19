606f59fd7c2804c5577d33dcba2b504a5acd3a78
Firxyz <109429190+Firxyz@users.noreply.github.com>
2022-07-19 20:58:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //════════════════════════════//
-//Recode By ArullOfc 
+//Recode By FirXyz 
 //Berasal dari sc Xeon Bot Inc. Cheems Bot MD
 //Thank you to Lord Buddha, Family and Myself
 //════════════════════════════//
@@ -24,23 +24,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc"
+global.owner = ['6285806459088'] //ur owner number
+global.ownername = "FirXyz"
 global.ytname = "YT:ARULL OFC " //ur yt chanel name
 global.socialm = "GitHub: Warikrr" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
 //bot bombdy
-global.botname = "ArullBotz" //ur bot name
+global.botname = "FirXyz" //ur bot name
 global.websitex = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
 global.packname = "Sticker By"
-global.author = "ArullBotz"
+global.author = "FirXyz"
 global.themeemoji = "📀"
 global.reactmoji = "🗿"
-global.ownertag = ['ArullOfc'] //ur owner tag
-global.ownernummenu = ['6285822347348'] //ur owner number in menu and all
-global.watermark = "ArullBotz" //ur watermark
+global.ownertag = ['FirXyz'] //ur owner tag
+global.ownernummenu = ['6285806459088'] //ur owner number in menu and all
+global.watermark = "FirXyz" //ur watermark
 global.botscript = "https://youtu.be/Gr6yuCbSGTI"
 global.linkz1 = "https://youtu.be/Gr6yuCbSGTI"
 global.linkz2 = "https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKASl"
